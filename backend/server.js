0d46fd5af3ad38879acada520419256faa720cc8
vansh <tomervansh.b777@gmail.com>
2025-04-05 00:40:51 +0530

    connected to database and made server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const port  = process.env.PORT || 4000;
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+
+const User = require('./models/user.model');
+app.listen(port, () => {    
+    console.log(`Server is running on port ${port}`);
+})
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+}
+).catch((err) => {
+    console.log('Error connecting to MongoDB:', err);
+})
+
