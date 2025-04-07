2228664e1c2d75dbb220a09fd4c99fabe3e41122
vriti62 <jaitleyvriti@gmail.com>
2025-04-07 15:51:20 +0530

    signup-endpoint

---
+++ backend/controllers/User.controller.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const user = require('../models/user.model');
+const bcrypt = require('bcrypt');
+
+exports.signup("/signup", async (req,res)=>{
+    try{
+
+        const {name,email,password} = req.body;
+        const existingUser = await findOne({email});
+
+        if(existingUser) return res.status(400).json("User already exists, please log in");
+
+        const hashedpassword = bcrypt.hash(password,10);
+
+        const newUser = new user({
+            name:name,
+            email:email,
+            password:hashedpassword
+        })
+
+        await newUser.save();
+        res.status(200).json("User created successfully :D")
+    }catch(err){
+        res.status(400).json(err);
+    }
+})
