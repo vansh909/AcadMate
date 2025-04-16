17609bdc7853ff7393ab01b954bfdc939a5e76b5
vriti62 <jaitleyvriti@gmail.com>
2025-04-17 00:26:25 +0530

    removed-teacherController

--- backend/routes/teacher.routes.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-const  teacherController  = require('../controllers/teacher.controller');
-const { isAuthenticated } = require('../middlewares/auth.middleware');
-const router = express.Router();
-
-router.get('/getStudentList', isAuthenticated, teacherController.getStudentList);
-
-
-module.exports =router;
