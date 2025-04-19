10e97fb177080788b2761a073dfe565ffe7df73a
vriti62 <jaitleyvriti@gmail.com>
2025-04-20 01:02:42 +0530

    added-classes-list-for-subject-teacher

--- backend/controllers/teacher.controller.js
+++ backend/controllers/teacher.controller.js
@@ -1,5 +1,6 @@
 const Classes = require('../models/class.model');
 const Students = require('../models/student.model');
+const mappings = require('../models/subject-teacher-mapping.model');
 
 
 exports.getStudentsList = async(req, res)=>{
@@ -21,3 +22,26 @@ exports.getStudentsList = async(req, res)=>{
         return res.status(500).json({Error:"Internal Server Error!"});
     }
 }
+
+exports.getClassLists = async (req, res) => {
+    try {
+        const user = req.user;
+
+        if (user.role !== 'teacher') {
+            return res.status(400).json({ Message: "Not Authorized!" });
+        }
+
+        const classes = await mappings.find({ teacherId: user._id })
+        .populate('classId', 'class_name total_students')
+        .populate('subjectId', 'subjectName');;
+
+        if (!classes || classes.length === 0) {
+            return res.status(404).json({ Message: "No classes assigned to you." });
+        }
+
+        return res.status(200).json({ Message: "Classes fetched successfully!", classes });
+    } catch (error) {
+        console.error("Error fetching class lists:", error);
+        return res.status(500).json({ Error: "Internal Server Error!" });
+    }
+};
--- backend/models/teacher.model.js
+++ backend/models/teacher.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const teacherSchema = new mongoose.Schema({
     teacherId:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:"user",
+        ref:"User",
         required:true
     },
     date_of_birth:{
