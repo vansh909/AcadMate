b1977fd6df4d15bb8c6b9335e650ea90be25a411
vriti62 <jaitleyvriti@gmail.com>
2025-04-23 15:37:31 +0530

    added multer and files in public folder/images/uploads

--- backend/routes/teacher.routes.js
+++ backend/routes/teacher.routes.js
@@ -2,10 +2,13 @@ const express= require('express');
 const {getStudentsList, getClassLists, getTeacherProfile} = require('../controllers/teacher.controller');
 const {isAuthenticated} = require('../middlewares/auth.middleware')
 const router = express.Router();
+const { uploadFile } = require('../controllers/teacher.controller');
+const upload = require('../config/multer');
 
 router.get('/StudentList',isAuthenticated, getStudentsList);
 router.get('/profile', isAuthenticated, getTeacherProfile);
-router.get('/ClassesList', isAuthenticated, getClassLists)
+router.get('/ClassesList', isAuthenticated, getClassLists);
+router.post('/uploadfile', upload.single('image'), uploadFile);
 
 
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,10 +2,14 @@ const express= require('express');
 const router = express.Router();
 
 const {signup, login}  = require('../controllers/User.controller');
-const {isAuthenticated} = require('../middlewares/auth.middleware')
+const {isAuthenticated} = require('../middlewares/auth.middleware');
+
 
 router.post('/signup',isAuthenticated, signup);
 router.post("/login",login);
+
+
+
 // In routes/user.js or wherever your /user/login route is
 router.post('/login', async (req, res) => {
     console.log('Login request received:', req.body);
