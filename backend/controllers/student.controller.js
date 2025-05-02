6423bcbd249231b9848f145797d320607e13b539
vriti62 <jaitleyvriti@gmail.com>
2025-05-02 23:51:24 +0530

    retreive attendance for student dashboard

--- backend/controllers/student.controller.js
+++ backend/controllers/student.controller.js
@@ -1,5 +1,5 @@
 const Student = require("../models/student.model");
-
+const Attendance =  require('../models/attendance.model');
 exports.studentProfile = async (req, res) => {
   const user = req.user;
   try {
@@ -15,5 +15,42 @@ exports.studentProfile = async (req, res) => {
   } catch (error) {
     console.error("Error fetching student profile:", error);
     return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.getAttendance = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const student = await Student.findOne({ student_id: userId }).populate("class_id", "class_name");
+    if (!student) {
+      return res.status(404).json({ Message: "Student not found" });
+    }
+
+    const attendanceRecords = await Attendance.find({ 
+      studentId: student.id, 
+      classId: student.classId 
+    });
+
+    if (!attendanceRecords) {
+      return res.status(404).json({ Message: "No attendance records found" });
+    }
+
+    // Calculate attendance summary
+    const totalDays = attendanceRecords.length;
+    const presentDays = attendanceRecords.filter(record => record.status === "present").length;
+    const absentDays = totalDays - presentDays;
+
+    // Return attendance summary along with records
+    return res.status(200).json({
+      Message: "Attendance fetched successfully",
+      Class: student.class_id,
+      TotalDays: totalDays,
+      PresentDays: presentDays,
+      AbsentDays: absentDays,
+      AttendanceRecords: attendanceRecords,
+    });
+  } catch (error) {
+    console.error("Error fetching attendance:", error);
+    return res.status(500).json({ Error: "Internal Server Error" });
   }
 };
