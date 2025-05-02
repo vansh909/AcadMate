5878b8b249bace6b6c40f3244a41c37e276a4f56
vriti62 <jaitleyvriti@gmail.com>
2025-05-02 23:51:24 +0530

    retreive attendance for student dashboard

--- backend/routes/student.routes.js
+++ backend/routes/student.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {studentProfile}= require('../controllers/student.controller')
+const {studentProfile, getAttendance}= require('../controllers/student.controller')
 
 const {isAuthenticated} = require('../middlewares/auth.middleware');
 
 router.get('/profile', isAuthenticated, studentProfile);
+router.get('/getAttendance', isAuthenticated, getAttendance);
 
 module.exports = router;
